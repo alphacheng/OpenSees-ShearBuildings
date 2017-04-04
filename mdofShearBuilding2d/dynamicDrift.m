db7096abae3b0ae0eca6c2e88a2e1cb18d10c14c
Peter Talley <peterctalley@gmail.com>
2017-04-04 12:29:28 -0400

    Added dynamicDrift.m

---
+++ mdofShearBuilding2d/dynamicDrift.m
@@ -0,0 +1,66 @@
+% dynamicDrift.m
+
+clear; close all; clc;
+
+%% Define Building
+nStories = 4;
+bldg = mdofShearBuilding2d(nStories);
+bldg.echoOpenSeesOutput = false;
+bldg.deleteFilesAfterAnalysis = false;
+bldg.pushover_stepSize = 0.001;
+bldg.pushover_maxDrift = 6.0;
+
+bldg.storyMass = [2 1 1 1];
+
+% Define Material
+K0 = 1000;      % elastic stiffness
+as = 0.04;      % strain hardening ratio
+My = 100;       % effective yield strength
+Lambda = 8;     % Cyclic deterioration parameter
+Fpr = 0.2;      % Ratio of (the force at which reloading begins)/(the force corresponding to the maximum historic deformation demand)
+A_Pinch = 0.2;  % Ratio of reloading stiffness
+c = 1;          % rate of deterioration
+theta_p = 0.3;  % pre-capping rotation
+theta_pc = 1.5; % post-capping rotation
+Res = 0.2;      % residual strength ratio
+theta_u = 1.9;  % ultimate rotation capacity
+D = 1.0;        % rate of cyclic deterioration
+nFactor = 0;    % elastic stiffness amplification factor
+
+materialDefinition = bilinearMaterialDefinition(K0,as,My,Lambda,c,theta_p,theta_pc,Res,theta_u,D,nFactor);
+% materialDefinition = peakOrientedMaterialDefinition(K0,as,My,Lambda,c,theta_p,theta_pc,Res,theta_u,D);
+% materialDefinition = pinchingMaterialDefinition(K0,as,My,Fpr,A_Pinch,Lambda,c,theta_p,theta_pc,Res,theta_u,D);
+
+bldg.storySpringDefinition = {
+    materialDefinition
+    sprintf('uniaxialMaterial Elastic 2 %g',K0)
+    sprintf('uniaxialMaterial Elastic 3 %g',K0)
+    sprintf('uniaxialMaterial Elastic 4 %g',K0)
+};
+
+%% Response History Analysis
+gmfile  = fullfile(pwd,'test.acc');
+dt      = 0.01;
+SF      = 200.0;
+tend    = 33.0;
+results = bldg.responseHistory(gmfile,dt,SF,tend);
+
+% Plot sample response history
+figure
+subplot(211)
+plot(results.time,results.groundMotion,'-')
+grid on
+grid minor
+xlabel('Time (s)')
+ylabel('Acceleration (g)')
+title('Input Ground Motion')
+
+subplot(212)
+plot(results.time,results.roofDrift,'-')
+grid on
+grid minor
+axisLimits = axis;
+axis([axisLimits(1:2),-max(abs(axisLimits(3:4))),max(abs(axisLimits(3:4)))])
+xlabel('Time (s)')
+ylabel('Roof drift')
+title('Roof Drift')
