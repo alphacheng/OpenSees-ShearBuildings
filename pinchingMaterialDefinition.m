970460dcf1cde6ebe5c8eb3e63a6967172040fc7
Peter Talley <peterctalley@gmail.com>
2017-04-04 11:42:22 -0400

    Added pinchingMaterialDefinition.m

---
+++ pinchingMaterialDefinition.m
@@ -0,0 +1,36 @@
+
+function materialDefinition = pinchingMaterialDefinition(K0, as, My, Fpr, A_Pinch, Lambda, c, theta_p, theta_pc, Res, theta_u, D)
+
+%K0;                        % elastic stiffness
+as_Plus = as;               % strain hardening ratio for positive loading direction
+as_Neg = as;                % strain hardening ratio for negative loading direction
+My_Plus = My;               % effective yield strength for positive loading direction
+My_Neg = -My;               % effective yield strength for negative loading direction (negative value)
+FprPos = Fpr;               % Ratio of the force at which reloading begins to force corresponding to the maximum historic deformation demand (positive loading direction)
+FprNeg = Fpr;               % Ratio of the force at which reloading begins to force corresponding to the absolute maximum historic deformation demand (negative loading direction)
+%A_Pinch;                   % Ratio of reloading stiffness
+Lambda_S = Lambda;          % Cyclic deterioration parameter for strength deterioration
+Lambda_C = Lambda;          % Cyclic deterioration parameter for post-capping strength deterioration
+Lambda_A = Lambda;          % Cyclic deterioration parameter for acceleration reloading stiffness deterioration (is not a deterioration mode for a component with Bilinear hysteretic response).
+Lambda_K = Lambda;          % Cyclic deterioration parameter for unloading stiffness deterioration
+c_S = c;                    % rate of strength deterioration. The default value is 1.0.
+c_C = c;                    % rate of post-capping strength deterioration. The default value is 1.0.
+c_A = c;                    % rate of accelerated reloading deterioration. The default value is 1.0.
+c_K = c;                    % rate of unloading stiffness deterioration. The default value is 1.0.
+theta_p_Plus = theta_p;     % pre-capping rotation for positive loading direction (often noted as plastic rotation capacity)
+theta_p_Neg = theta_p;      % pre-capping rotation for negative loading direction (often noted as plastic rotation capacity) (positive value)
+theta_pc_Plus = theta_pc;   % post-capping rotation for positive loading direction
+theta_pc_Neg = theta_pc;    % post-capping rotation for negative loading direction (positive value)
+Res_Pos = Res;              % residual strength ratio for positive loading direction
+Res_Neg = Res;              % residual strength ratio for negative loading direction (positive value)
+theta_u_Plus = theta_u;     % ultimate rotation capacity for positive loading direction
+theta_u_Neg = theta_u;      % ultimate rotation capacity for negative loading direction (positive value)
+D_Plus = D;                 % rate of cyclic deterioration in the positive loading direction (this parameter is used to create asymmetric hysteretic behavior for the case of a composite beam). For symmetric hysteretic response use 1.0.
+D_Neg = D;                  % rate of cyclic deterioration in the negative loading direction (this parameter is used to create asymmetric hysteretic behavior for the case of a composite beam). For symmetric hysteretic response use 1.0.
+
+materialDefinition = sprintf('uniaxialMaterial ModIMKPinching 1 %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g %g',...
+    K0,as_Plus,as_Neg,My_Plus,My_Neg,FprPos,FprNeg,A_Pinch,Lambda_S,Lambda_C,Lambda_A,Lambda_K,...
+    c_S,c_C,c_A,c_K,theta_p_Plus,theta_p_Neg,theta_pc_Plus,theta_pc_Neg,...
+    Res_Pos,Res_Neg,theta_u_Plus,theta_u_Neg,D_Plus,D_Neg);
+
+end
