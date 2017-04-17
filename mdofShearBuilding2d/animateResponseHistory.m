cbc02b4e3751dd8eb23ef9712477b16ede070ed7
Peter Talley <peterctalley@gmail.com>
2017-04-17 13:37:56 -0400

    Switch to using actual story heights

--- mdofShearBuilding2d/animateResponseHistory.m
+++ mdofShearBuilding2d/animateResponseHistory.m
@@ -1,6 +1,11 @@
 %% Animate response history
 
-function animateResponseHistory( results  )
+function animateResponseHistory( results, heights  )
+
+cumHeights = zeros(1,length(heights));
+for i = 1:length(heights)
+    cumHeights(i) = sum(heights(1:i));
+end
 
 figure
 
@@ -8,25 +13,24 @@ grid on
 grid minor
 
 dt = max(diff(results.time));
-nStories = size(results.totalDrift,2);
 xMax = max(max(abs(results.totalDrift)));
-yMax = nStories+1;
+yMax = sum(heights) + heights(1);
 
 axis([-xMax xMax 0 yMax]);
 ax = gca;
-ax.YTick = 0:nStories;
+ax.YTick = cumHeights;
 
-ylabel('Story')
+ylabel('Height')
 xlabel('Drift')
 
-h = animatedline([0 results.totalDrift(1,:)],0:nStories,'Marker','*');
+h = animatedline([0 results.totalDrift(1,:)],[0 cumHeights],'Marker','*');
 t = text(2/3*xMax,1/5*yMax,'Time: 0.0s');
 
 for i = 1:size(results.totalDrift,1)
     displayText = sprintf('Time: %4.1fs',i*dt);
     delete(t)
     clearpoints(h)
-    addpoints(h,[0 results.totalDrift(i,:)],0:nStories)
+    addpoints(h,[0 results.totalDrift(i,:)],[0 cumHeights])
     t = text(ax,2/3*xMax,1/5*yMax,displayText);
     drawnow
 end
