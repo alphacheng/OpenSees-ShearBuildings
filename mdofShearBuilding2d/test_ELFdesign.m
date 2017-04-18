291ea5a414e71758dcbb642f9bac6b12436b5130
Peter Talley <peterctalley@gmail.com>
2017-04-17 21:04:36 -0400

    Lots of work and reorganizing

--- mdofShearBuilding2d/test_ELFdesign.m
+++ mdofShearBuilding2d/test_ELFdesign.m
@@ -10,12 +10,13 @@ nStories = 3;
 bldg = mdofShearBuilding2d(nStories);
 bldg.echoOpenSeesOutput = false;
 bldg.deleteFilesAfterAnalysis = true;
-bldg.g = 32.2;
+verbose = true;                                 % Verbose output (bool)
+bldg.g = 32.2;                                  % Acceleration due to gravity (ft/s^2)
 
-bldg.storyHeight = [20 15 15];
-storyDL = [0.080 0.080 0.030];
-storyArea = 90*90*ones(1,nStories);
-bldg.storyMass = (storyDL .* storyArea)/bldg.g;
+bldg.storyHeight = [20 15 15];                  % Story heights (ft)
+storyDL = [0.080 0.080 0.030];                  % Story dead loads (ksf)
+storyArea = 90*90*ones(1,nStories);             % Story areas (ft^2)
+bldg.storyMass = (storyDL .* storyArea)/bldg.g; % Story masses (kslug)
 
 %% Design building
 bldg.seismicDesignCategory = 'Dmax';
@@ -35,74 +36,75 @@ Fpr = 0.2;      % Ratio of (the force at which reloading begins)/(the force corr
 A_Pinch = 0.2;  % Ratio of reloading stiffness
 c = 1;          % rate of deterioration
 theta_p = zeros(nStories,1);  % pre-capping rotation
-theta_pc = 1.5; % post-capping rotation
 Res = 0.2;      % residual strength ratio
-theta_u = 1.9;  % ultimate rotation capacity
 D = 1.0;        % rate of cyclic deterioration
 nFactor = 0;    % elastic stiffness amplification factor
 
 py_factor = 3;  % ratio of theta_p to theta_y
+theta_y = zeros(nStories,1);
 for i = 1:nStories
     Solution = [-py_factor 1 0 ; 0 as*K0(i) 1 ; 1 0 -1/K0(i)]^-1 * [0 ; resultsELF.designStrength(i) ; 0];
-    % theta_y(i) = Solution(1); % rotation at yield
+    theta_y(i) = Solution(1); % rotation at yield
     theta_p(i) = Solution(2); % pre-capping rotation
     M_y(i)     = Solution(3); % effective yield strength
 end
 
+theta_pc = 4*theta_p;                       % post-capping rotation
+theta_u = theta_y + theta_p + 2/3*theta_pc; % ultimate rotation capacity
+
 bldg.storySpringDefinition = cell(nStories,1);
 for i = 1:nStories
-    bldg.storySpringDefinition{i} = bilinearMaterialDefinition(i,K0(i),as,M_y(i),Lambda,c,theta_p(i),theta_pc,Res,theta_u,D,nFactor);
+    bldg.storySpringDefinition{i} = bilinearMaterialDefinition(i,K0(i),as,M_y(i),Lambda,c,theta_p(i),theta_pc(i),Res,theta_u(i),D,nFactor);
 end
 
 %% Response History Analysis
 load('ground_motions.mat');
-gmfile = bldg.scratchFile(sprintf('acc%s.acc',ground_motions(1).ID));
-gmfid = fopen(gmfile,'w+');
-for k = 1:ground_motions(1).numPoints
-    fprintf(gmfid,'%g\n',ground_motions(1).normalized_acceleration(k)*bldg.g);
+SF2 = [0:.25:1.5, 2:0.5:6, 6.75:0.75:9, 10:12];
+SMT = FEMAP695_SMT(bldg.fundamentalPeriod,bldg.seismicDesignCategory);
+ST  = SMT*SF2;
+figure
+hold on
+nMotions = 6;
+legendentries = cell(nMotions,1);
+results = cell(nMotions,length(SF2));
+for i = 1:nMotions %length(ground_motions)
+    gmfile = bldg.scratchFile(sprintf('acc%s.acc',ground_motions(i).ID));
+    gmfid = fopen(gmfile,'w+');
+    for k = 1:ground_motions(i).numPoints
+        fprintf(gmfid,'%f\n',ground_motions(i).normalized_acceleration(k)*bldg.g);
+    end
+    fclose(gmfid);
+    dt      = ground_motions(i).dt;
+    SF1     = FEMAP695_SF1(bldg.fundamentalPeriod,bldg.seismicDesignCategory);
+    tend    = max(ground_motions(i).time) + 5;
+
+    % Vary scale factor
+
+    maxDrift = zeros(length(SF2),1);
+    parfor j = 1:length(SF2)
+        if verbose
+            fprintf('Calculating IDA point for %s, SF2 = %g\n',ground_motions(i).ID,SF2(j));
+        end
+        SF = SF1*SF2(j);
+        results{i,j} = bldg.responseHistory(gmfile,dt,SF,tend,ground_motions(i).ID,j);
+        maxDrift(j) = max(max(abs(results{i,j}.storyDrift))./bldg.storyHeight);
+    end
+
+    plot(maxDrift,ST,'o-')
+    legendentries{i} = ground_motions(i).ID;
+
+    if bldg.deleteFilesAfterAnalysis
+        delete(gmfile)
+    end
 end
-dt      = ground_motions(1).dt;
-SF1     = FEMAP695_SF1(bldg.fundamentalPeriod,bldg.seismicDesignCategory);
-SF      = SF1*12;
-tend    = max(ground_motions(1).time) + 5;
-results = bldg.responseHistory(gmfile,dt,SF,tend,1);
+
+
+grid on
+xlabel('Maximum story drift ratio')
+ylabel('Ground motion intensity, S_T (g)')
+legend(legendentries)
 
 % Plot sample response history
-figure
-subplot(211)
-plot(results.time,results.groundMotion,'-')
-grid on
-grid minor
-xlabel('Time (s)')
-ylabel('Acceleration (ft/s^2)')
-titleText = sprintf('Input Ground Motion (SF = %g)',SF);
-title(titleText)
-
-subplot(212)
-plot(results.time,results.roofDrift,'-')
-grid on
-grid minor
-axisLimits = axis;
-axis([axisLimits(1:2),-max(abs(axisLimits(3:4))),max(abs(axisLimits(3:4)))])
-xlabel('Time (s)')
-ylabel('Drift (ft)')
-title('Roof Drift')
-
-% Vary scale factor
-SF2 = [0:.25:1.5, 2:0.5:6, 6.75:0.75:9, 10:12];
-maxDrift = zeros(length(SF),1);
-parfor i = 1:length(SF2)
-    fprintf('Calculating IDA point for SF2 = %g\n',SF2(i));
-    SF = SF1*SF2(i);
-    results = bldg.responseHistory(gmfile,dt,SF,tend,i);
-    maxDrift(i) = max(max(abs(results.storyDrift)));
-end
-
-figure
-ST = FEMAP695_SMT(bldg.fundamentalPeriod,bldg.seismicDesignCategory)*SF2;
-plot(maxDrift,ST,'o-')
-grid on
-xlabel('Maximum story drift')
-ylabel('Ground motion intensity, S_T')
+plotSampleResponse(results{1,5})
 
 toc
