f1adecd9093021d2ce8b171ddbf9c2defed3505f
Peter Talley <peterctalley@gmail.com>
2017-05-11 15:40:03 -0400

    Fold animateResponseHistory.m into mdofShearBuilding2d.m

--- mdofShearBuilding2d/mdofShearBuilding2d.m
+++ mdofShearBuilding2d/mdofShearBuilding2d.m
@@ -521,6 +521,43 @@ classdef mdofShearBuilding2d < OpenSeesAnalysis
             end
 
         end %function:springDesign
+
+        function animateResponseHistory(obj,results)
+
+            cumHeights = zeros(1,length(obj.storyHeight));
+            for i = 1:length(obj.storyHeight)
+                cumHeights(i) = sum(obj.storyHeight(1:i));
+            end
+
+            figure
+
+            grid on
+            grid minor
+
+            dt = max(diff(results.time));
+            xMax = max(max(abs(results.totalDrift)));
+            yMax = sum(obj.storyHeight) + obj.storyHeight(1);
+
+            axis([-xMax xMax 0 yMax]);
+            ax = gca;
+            ax.YTick = cumHeights;
+
+            ylabel(sprintf('Height (%s)',obj.units.length))
+            xlabel(sprintf('Drift (%s)',obj.units.length))
+
+            h = animatedline([0 results.totalDrift(1,:)],[0 cumHeights],'Marker','*');
+            t = text(2/3*xMax,1/5*yMax,'Time: 0.0s');
+
+            for i = 1:size(results.totalDrift,1)
+                displayText = sprintf('Time: %4.1fs',i*dt);
+                delete(t)
+                clearpoints(h)
+                addpoints(h,[0 results.totalDrift(i,:)],[0 cumHeights])
+                t = text(ax,2/3*xMax,1/5*yMax,displayText);
+                drawnow
+            end
+
+        end
     end %methods
 end %classdef:mdofShearBuilding2d
 
