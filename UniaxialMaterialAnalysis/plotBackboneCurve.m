09a98f0b15a3af14c0fd0df871d251c2704e8aee
Peter Talley <peterctalley@gmail.com>
2017-05-15 15:36:43 -0400

    Remove forced black line

--- UniaxialMaterialAnalysis/plotBackboneCurve.m
+++ UniaxialMaterialAnalysis/plotBackboneCurve.m
@@ -13,6 +13,6 @@ results_pos_env = anaobj.runAnalysis([0 1.1*theta_u],rateType,rateValue);
 if newfig
     figure
 end
-plot(results_pos_env.disp,results_pos_env.force,'k')
+plot(results_pos_env.disp,results_pos_env.force)
 
 end
