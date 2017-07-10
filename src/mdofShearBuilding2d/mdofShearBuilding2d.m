b45c3f2bbcd144e5744057500c3ef0fc412a2d25
Peter Talley <peterctalley@gmail.com>
2017-07-10 00:56:51 -0400

    Move the energy test to inside the class where it belongs

--- src/mdofShearBuilding2d/mdofShearBuilding2d.m
+++ src/mdofShearBuilding2d/mdofShearBuilding2d.m
@@ -757,6 +757,23 @@ methods
 
     end
 
+    function E_EQ = energyTest(obj,results)
+    % ENERGYTEST
+
+        time = results.time;
+
+        M = obj.storyMass(:);
+
+        relativeVeloc = results.totalVeloc;
+
+        u_dot = relativeVeloc;
+        u_ddot_eq = results.groundMotion;
+
+        expression = u_dot*M.*u_ddot_eq;
+
+        E_EQ = -cumtrapz(time,expression);
+
+    end
 
     function results = ELFanalysis(obj)
         %% ELFANALYSIS Equivalent Lateral Force procedure (ASCE 7-10)
