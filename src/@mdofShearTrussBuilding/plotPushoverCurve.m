9f8f549b4c99a2161b25805da6a7429f3b7011a9
Peter Talley <peterctalley@gmail.com>
2017-07-25 15:02:20 -0400

    Modify `plotPushoverCurve` to allow multiple curves on one fig

--- src/@mdofShearTrussBuilding/plotPushoverCurve.m
+++ src/@mdofShearTrussBuilding/plotPushoverCurve.m
@@ -1,13 +1,17 @@
-function fig = plotPushoverCurve(obj,results)
+function fig = plotPushoverCurve(obj,results,fig)
 
-fig = figure;
-hold on
-plot(results.roofDrift,results.baseShear,'-')
-grid on
-grid minor
-xlabel(sprintf('Roof drift (%s)',obj.units.length))
-ylabel(sprintf('Base shear (%s)',obj.units.force))
-title('Pushover analysis')
+if nargin == 2
+    fig = figure;
+    hold on
+    grid on
+    grid minor
+    xlabel(sprintf('Roof drift (%s)',obj.units.length))
+    ylabel(sprintf('Base shear (%s)',obj.units.force))
+    title('Pushover analysis')
+end
+
+ax = fig.Children;
+plot(ax,results.roofDrift,results.baseShear,'-')
 
 if nargout == 0
     clear fig
