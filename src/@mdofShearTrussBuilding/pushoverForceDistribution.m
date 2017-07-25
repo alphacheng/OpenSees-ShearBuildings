e2dde553a9f0db14d5e283828694c90109becfc6
Peter Talley <peterctalley@gmail.com>
2017-07-25 15:05:08 -0400

    Add separate force distribution function

---
+++ src/@mdofShearTrussBuilding/pushoverForceDistribution.m
@@ -0,0 +1,18 @@
+function F = pushoverForceDistribution(obj)
+%% PUSHOVERFORCEDISTRIBUTION Calculate the force ratios for pushover analysis.
+%
+%   F = pushoverForceDistribution(obj) calculates the force ratios based on the
+%       first mode of vibration and the mass distribution.
+%
+%   Ref. FEMA P695 Equation 6-4.
+%
+
+[~,eigenvecs] = obj.eigenvalues;
+
+F = obj.storyMass' .* eigenvecs;
+if min(F) < 0
+    F = -F;
+end
+F = F/max(F);
+
+end
