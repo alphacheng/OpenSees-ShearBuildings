0abe9299294789c55501b67563c0e444a4be3907
Peter Talley <peterctalley@gmail.com>
2017-10-10 01:59:38 -0400

    Add strongback nodes to animateResponseHistory

--- src/@Strongback/animateResponseHistory.m
+++ src/@Strongback/animateResponseHistory.m
@@ -10,7 +10,7 @@ function animateResponseHistory(obj,results,dt)
     grid on
     grid minor
 
-    xMax = max(max(abs(results.displacement_x)));
+    xMax = max(max(abs(results.displacement_x)))*1.2;
     yMax = sum(obj.storyHeight) + obj.storyHeight(1);
 
     axis([-xMax xMax 0 yMax]);
@@ -20,19 +20,27 @@ function animateResponseHistory(obj,results,dt)
     ylabel(sprintf('Height (%s)',obj.units.length))
     xlabel(sprintf('Drift (%s)',obj.units.length))
 
-    xPos = [0 results.displacement_x(1,:)];
+    xPos = [0 results.displacement_x(1,:)]-xMax/6;
     yPos = [0 cumHeights] + [0 results.displacement_y(1,:)];
 
+    xPos_sb = xPos+xMax/3;
+    yPos_sb = [0 cumHeights] + [0 results.strongbackDispY(1,:)];
+
     h = animatedline(xPos,yPos,'Marker','*');
+    h2 = animatedline(xPos_sb,yPos_sb,'Marker','*');
     t = text(2/3*xMax,1/5*yMax,'Time: 0.0s');
 
     for i = 1:size(results.displacement_x,1)
         displayText = sprintf('Time: %4.1fs',i*dt);
         delete(t)
         clearpoints(h)
-        xPos = [0 results.displacement_x(i,:)];
+        clearpoints(h2)
+        xPos = [0 results.displacement_x(i,:)]-xMax/6;
         yPos = [0 cumHeights] + [0 results.displacement_y(i,:)];
+        xPos_sb = xPos+xMax/3;
+        yPos_sb = [0 cumHeights] + [0 results.strongbackDispY(i,:)];
         addpoints(h,xPos,yPos)
+        addpoints(h2,xPos_sb,yPos_sb)
         t = text(ax,2/3*xMax,1/5*yMax,displayText);
         drawnow
     end
